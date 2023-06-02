84177f6eb59060d56b3ade1acf5f5f4738afb9dc
jhonatan.dev1 <jhonatan.dev@agroflux.ind.br>
2023-06-02 11:19:02 -0300

    feat: inserindo regras de lint

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,4 +1,45 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint',
+        'react',
+        'prettier',
+        'react-hooks',
+        'react-perf',
+    ],
+    rules: {
+        'no-console': 'warn',
+        'prettier/prettier': 'error',
+        'max-len': ['warn', 120],
+        'import/prefer-default-export': 'off',
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'react-perf/jsx-no-new-object-as-prop': 'warn',
+        'react-perf/jsx-no-new-array-as-prop': 'warn',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        // if you use React 17+; otherwise, turn this on
+        'react/react-in-jsx-scope': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'no-undef': 'off',
+    },
+    ignorePatterns: ['node_modules'],
 };
--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,51 +5,58 @@
  * @format
  */
 
-import React from 'react';
+import React, {useMemo} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  useColorScheme,
-  View,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    useColorScheme,
+    View,
 } from 'react-native';
 import * as eva from '@eva-design/eva';
 import {ApplicationProvider} from '@ui-kitten/components';
 
 import {
-  Colors,
-  Header,
-  LearnMoreLinks,
+    Colors,
+    Header,
+    LearnMoreLinks,
 } from 'react-native/Libraries/NewAppScreen';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const backgroundStyle = useMemo(
+        () => ({
+            backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        }),
+        [isDarkMode],
+    );
 
-  return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <Header />
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            }}>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </ApplicationProvider>
-  );
+    const viewStyle = useMemo(
+        () => ({
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        }),
+        [isDarkMode],
+    );
+
+    return (
+        <ApplicationProvider {...eva} theme={eva.light}>
+            <SafeAreaView style={backgroundStyle}>
+                <StatusBar
+                    barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                    backgroundColor={backgroundStyle.backgroundColor}
+                />
+                <ScrollView
+                    contentInsetAdjustmentBehavior="automatic"
+                    style={backgroundStyle}>
+                    <Header />
+                    <View style={viewStyle}>
+                        <LearnMoreLinks />
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        </ApplicationProvider>
+    );
 }
 
 export default App;
