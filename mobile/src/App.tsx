1a701ffcf9ed83c1a365320dd69453a4dd1b23ab
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2023-06-21 08:31:59 -0300

    feat: inserindo onesignal

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -5,12 +5,36 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { PermissionsAndroid } from 'react-native';
 import { AppStack } from './routes/AppStack';
 
 function App(): JSX.Element {
+    const requestCameraPermission = async () => {
+        try {
+            const teste = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+                {
+                    title: 'Permissões de notificações',
+                    message:
+                        'Necessário para receber notificações sobre suas máquinas.',
+                    buttonNeutral: 'Pergunte depois',
+                    buttonNegative: 'Cancelar',
+                    buttonPositive: 'Aceitar',
+                },
+            );
+            console.log('PermissionsAndroid POST_NOTIFICATIONS', teste);
+        } catch (err) {
+            console.warn(err);
+        }
+    };
+
+    useEffect(() => {
+        requestCameraPermission();
+    }, []);
+
     return (
         <NavigationContainer>
             <AppStack />
