a7b27f2364cdb6ab8b9880253f03416579ed1674
JessePires <jessepires2010@gmail.com>
2023-06-30 23:37:31 -0300

    docs: Adicionando doc no service de cluster

--- backend/src/app.cluster.service.ts
+++ backend/src/app.cluster.service.ts
@@ -6,11 +6,19 @@ const cluster = _cluster as unknown as _cluster.Cluster;
 
 const numCPUs = os.cpus().length;
 
+/**
+ * Clusteriza a aplicação, permitindo a execução de múltiplos processos em paralelo.
+ * O callback é executado apenas pelos workers secundários.
+ *
+ * @param {Function} callback - O callback a ser executado pelos workers secundários.
+ * @returns {void}
+ */
 @Injectable()
 export default class AppClusterService {
   static clusterize(callback: Function): void {
     if (cluster.isPrimary) {
       console.log(`Master server started on ${process.pid}`);
+      // eslint-disable-next-line no-plusplus
       for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
       }
